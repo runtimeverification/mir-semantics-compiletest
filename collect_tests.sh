7cb4786d4aa07464864840833030e2009400e1ef
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-05-16 16:40:00 -0400

    improve collect_tests script

--- collect_tests.sh
+++ collect_tests.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 set -e
-die() { echo "$1"; exit 1; }
+print_usage() {
+  printf "usage: $(basename $0) [-x] [-e] [-f] [-t] [-h]\n\n"
+  printf "print out rustc UI tests which satisfy certain criteria; by default those that are:\n"
+  printf "normal, passing, contain a non-empty main function, and compile in reasonable time\n\n"
+  printf "options:\n"
+  printf "x\tinclude fixed tests [may contain run-rustfix header]\n"
+  printf "e\tinclude tests with empty/missing main function\n"
+  printf "f\tinclude failing tests [may NOT contain run-pass header] (implies -t)\n"
+  printf "t\tinclude tests that fail to generate MIR within 30s\n"
+  printf "h\tprint this usage and exit\n\n"
+  echo "$@"
+  exit 1
+}
 
 # setup time mir filter
 # NOTE: temp file is needed because rustc chokes when writing directly to /dev/null
@@ -16,26 +28,7 @@ RUNPASS=('-execdir' 'grep' '-q'  '//@[[:space:]]*run-pass'                      
 HASMAIN=('-execdir' 'grep' '-q'  'fn main[[:space:]]*([[:space:]]*)'                           '{}' ';')
 EMPMAIN=('-execdir' 'grep' '-qv' 'fn main[[:space:]]*([[:space:]]*)[[:space:]]*{[[:space:]]*}' '{}' ';')
 
-print_usage() {
-  printf "usage: $(basename $0) [-x] [-e] [-f] [-t] [-h]\n\n"
-  printf "print out rustc UI tests which satisfy certain criteria; by default those that are:\n"
-  printf "normal, passing, contain a non-empty main function, and compile in reasonable time\n\n"
-  printf "options:\n"
-  printf "x\tinclude fixed tests [may contain run-rustfix header]\n"
-  printf "e\tinclude tests with empty/missing main function\n"
-  printf "f\tinclude failing tests [may NOT contain run-pass header] (implies -t)\n"
-  printf "t\tinclude tests that fail to generate MIR within 30s\n"
-  printf "h\tprint this usage and exit\n\n"
-  printf "environment variable RUST_TOP must point to a valid rustc distribution\n"
-  exit 1
-}
-
-[ $# -eq 0 ] && print_usage
-
-[ -z "${RUST_TOP}" ] && die "Must set RUST_TOP environment variable to your local Rust compiler source directory before running this script"
-RUST_TESTS=${RUST_TOP}/tests
-[ ! -d "${RUST_TESTS}" ] && die "RUST_TOP environment variable does not appear to point to a local Rust compiler source directory"
-
+# parse opts
 while getopts 'xfet' opt; do
   case "${opt}" in
     x)   RUSTFIX=()  ;;
@@ -44,11 +37,16 @@ while getopts 'xfet' opt; do
     f)   RUNPASS=()
          TIMEMIR=()  ;;
     t)   TIMEMIR=()  ;;
-    ?|h) print_usage ;;
+    ?|h) print_usage "environment variable RUST_TOP must point to a valid rustc distribution" ;;
   esac
 done
 
-# find files that pass filters
+# check test dir
+[ -z "${RUST_TOP}" ] && print_usage "set RUST_TOP environment variable to your local Rust compiler source directory before running this script"
+RUST_TESTS=${RUST_TOP}/tests
+[ ! -d "${RUST_TESTS}" ] && print_usage "RUST_TOP environment variable does not appear to point to a local Rust compiler source directory"
+
+# find test files that pass filters
 find "${RUST_TESTS}/ui"
      -name '*.rs'    \
      "${RUSTFIX[@]}" \
